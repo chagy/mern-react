dcc0942eef5736cd1545377bf7b9b32cdb2fd978
chagy <chagy_x_@hotmail.com>
2021-05-03 15:34:58 +0700

    Backend product controller & routes & seeder

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
-require("dotenv").config()
-const express = require('express')
-const connectDB = require("./config/db")
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+const productRoutes = require("./routes/product");
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 5000
+app.use(express.json());
 
-app.listen(PORT,() => console.log(`Server running on port ${PORT}`))
+app.use("/api/products", productRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
